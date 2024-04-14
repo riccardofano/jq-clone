cde0d7b5b8f3472d737e8f67cc01dd6c9a9d29b9
Riccardo Fano <rcr.fano@gmail.com>
2024-04-14 14:09:48 +0200

    Apply identity token

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use serde_json::Value;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) enum Token<'a> {
     Identity,
@@ -6,4 +8,36 @@ pub(crate) enum Token<'a> {
     OptionalIndex(usize),
     OptionalKey(&'a str),
     Iterator,
+}
+
+fn apply_tokens(input: &Value, tokens: &[Token<'_>]) -> Value {
+    let mut output = input;
+
+    for token in tokens {
+        match token {
+            Token::Identity => {}
+            Token::Index(_) => todo!(),
+            Token::Key(_) => todo!(),
+            Token::OptionalIndex(_) => todo!(),
+            Token::OptionalKey(_) => todo!(),
+            Token::Iterator => todo!(),
+        }
+    }
+
+    output.to_owned()
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn apply_identity_token() {
+        let input = json!({"quotes": ["a", "b", "c"]});
+        let tokens = vec![Token::Identity];
+
+        assert_eq!(apply_tokens(&input, &tokens), input);
+    }
 }
